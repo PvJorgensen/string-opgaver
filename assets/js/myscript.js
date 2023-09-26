5276498ab5410b7874a146113728f983f4395f48
Patrik_Jørgensen <vadmand806@gmail.com>
2023-09-26 10:25:31 +0200

    Update myscript.js

---
+++ assets/js/myscript.js
@@ -0,0 +1,64 @@
+//Sammensætning af navn:
+//Opret to variabler, fornavn og efternavn, og tildel dem dine fornavn og efternavn. 
+//Lav en ny variabel, fuldtNavn, der indeholder hele dit navn ved at sammensætte fornavn og efternavn. Udskriv fuldtNavn til konsollen.
+
+//Kode her
+let fornavn = "Patrik";
+let efternavn ="Jørgensen";
+
+let fuldtNavn = fornavn + " " + efternavn;
+
+console.log(fuldtNavn);
+
+
+//Velkomstbesked:
+//Lav en variabel, brugernavn, og tildel den værdien af dit valgte brugernavn. 
+//Opret en velkomstbesked, der siger "Velkommen, [brugernavn]!" ved at sammensætte strengen "Velkommen, " og brugernavn. 
+//Udskriv velkomstbeskeden til konsollen.
+
+//Kode her
+let brugernavn = "Patrikabe";
+
+let velkomstbesked = "Velkommen, " + brugernavn + "!";
+
+console.log(velkomstbesked);
+
+
+//Bynavn og Postnummer:
+//Opret to variabler, by og postnummer, og tildel dem værdierne af din by og dit postnummer. 
+//Brug string + string metoden til at sammensætte dem og gem resultatet i en ny variabel kaldet adresse. Udskriv adresse til konsollen.
+
+//Kode her
+let by = "Aalborg";
+let postnummer = "9000";
+
+let adresse = by + " " + postnummer;
+
+console.log(adresse);
+
+
+//Ferieplanlægning:
+//Lav en variabel, ferieDestination, og tildel den værdien af dit ønskede feriemål. Opret en anden variabel, feriePlan, 
+//der indeholder en besked, der siger: "Jeg planlægger at tage til [ferieDestination] i næste uge." 
+//Brug string + string metoden til at sammensætte beskeden og udskriv den til konsollen.
+
+//Kode her
+let ferieDestination = "Sverige";
+
+let feriePlan = "Jeg planlægger at tage til " + ferieDestination + " i næste uge";
+
+console.log(feriePlan);
+
+
+//Produktnavn og Pris:
+//Opret to variabler, produktNavn og produktPris, og tildel dem værdierne af et produkt og dets pris. 
+//Lav en besked, der siger: "Det valgte produkt er [produktNavn], og det koster [produktPris] dollars." 
+//Brug string + string metoden til at sammensætte beskeden og udskriv den til konsollen.
+
+//Kode her
+let produktNavn = "Sandwich"
+let produktPris = 20
+
+let besked = "Det valgte produkt er " + produktNavn + " og det koster " + produktPris + " kr";
+
+console.log(besked);
